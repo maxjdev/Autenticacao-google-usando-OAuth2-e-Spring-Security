f35735e3c09066d2cb1b45f81992163f0846c61f
maxjdev <maxjramosdev@gmail.com>
2024-09-03 23:56:17 -0300

    initial settings

---
+++ src/test/java/br/com/spSec/SpSecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
