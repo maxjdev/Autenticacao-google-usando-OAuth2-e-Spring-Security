6478dad5d9a45136c79b3db88a46830d9326b0bd
maxjdev <maxjramosdev@gmail.com>
2024-09-03 23:58:34 -0300

    feat: add main class

---
+++ src/main/java/br/com/spSec/SpSecApplication.java
@@ -0,0 +1,13 @@
+package br.com.spSec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpSecApplication.class, args);
+	}
+
+}
